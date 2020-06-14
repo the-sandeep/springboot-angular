60066ed7dc8abe0591d92a74cc08cbbe981c92c6
Sandeep Maurya <sandeep.maurya018@gmail.com>
2020-06-14 15:23:06 +0530

    first commit

---
+++ src/test/java/com/saan/SpringbootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
